02142b9a2ffe7f43a4213f2e88069ae22ee1d7b6
Sergio López <sergio.lopez@holasoft.es>
2023-08-13 08:20:59 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https//lbsergiohs.github.io/cbgar/',
+  base: 'https//LBSergioHS.github.io/cbgar/',
   plugins: [react()],
 })
