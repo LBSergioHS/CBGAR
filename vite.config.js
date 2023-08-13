a7d02ea73365fad9bee93ded77b2ef0d3d5b2c19
Sergio López <sergio.lopez@holasoft.es>
2023-08-13 07:59:53 +0200

    Configuración en minúsculas

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https//LBSergioHS.github.io/',
+  base: 'https//lbsergiohs.github.io/',
   plugins: [react()],
 })
